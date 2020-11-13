3159e1c0d27637e7c6c524739c9c38a242226def
Yu <t102518034@gmail.com>
2020-11-13 14:37:24 +0800

    Initial Commit

---
+++ RedBeard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedBeard
+//
+//  Created by 陳冠諭 on 2020/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
